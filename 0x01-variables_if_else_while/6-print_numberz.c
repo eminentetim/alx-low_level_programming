4dd32eccf17665e80172f75f71bda34130a9d1ee
eminentetim <etimeminent@gmail.com>
2023-06-16 16:54:47 -0700

    updating

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - void
+ * Return: is 0 for success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+		putchar('\n');
+	}
+	/* is working */
+	return (0);
+}
