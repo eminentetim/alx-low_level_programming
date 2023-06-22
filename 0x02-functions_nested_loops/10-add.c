c615dbdd0e34513e4d002f9fbacf2e41848c62ff
eminentetim <etimeminent@gmail.com>
2023-06-22 04:38:22 -0700

    writing a function that print two integer

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * @n1: The first integer to be added
+ * @n2: The second integer to be added
+ * Return: the sum of the two integers
+ */
+
+int add(int n1, int n2)
+{
+	int result = n1 + n2;
+
+	return (result);
+}
