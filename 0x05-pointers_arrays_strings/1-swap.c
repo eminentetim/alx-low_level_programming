1d409543da52ba21839b7ffaf81e028789fd33d7
eminentetim <etimeminent@gmail.com>
2023-06-26 07:20:18 -0700

    creating a function that swap the value of two variable

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* swap_int(int *a, int *b) - swicth the value of a and b
+* swap_int - void
 * @a: hold the value of integer
 * @b: holds the value of the second integer
 * Return: 0
