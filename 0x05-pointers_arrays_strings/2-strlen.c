c47f1ae46ee650f72a9376ddc42c009ed10b3b27
eminentetim <etimeminent@gmail.com>
2023-06-26 08:32:00 -0700

    creating a function that count the lenght of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,22 +3,19 @@
 
 /**
  * _strlen - returns the length of a string
- * @str: string to calculate length of
+ * @s: string to calculate length of
  *
  * Return: length of string
  */
 
-int _strlen(char *str)
+int _strlen(char *s)
 
 {
 	int len = 0;
 
-	str = "My first strlen!";
-
-	while (*str != '0')
+	while (*s++)
 	{
 		len++;
-		str++;
 	}
 	return (len);
 }
