222eae7b5f7f6987a9a93b414cb81ba95f46b170
eminentetim <etimeminent@gmail.com>
2023-06-29 14:27:41 -0700

    creating a function that print integer

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,39 +1,51 @@
 #include "main.h"
+
+/**
+ * infinite_add - Concatenates two strings.
+ * @n1: Pointer to the destination string.
+ * @r: Pointer to the source string.
+ * @n2: hold the vale of src in bytes
+ * @size_r: set the size
+ * Return: Pointer to the resulting concatenated string.
+ */
+
+
+
 #include <string.h>
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int len1 = strlen(n1);
-        int len2 = strlen(n2);
-        int max_len = (len1 > len2) ? len1 : len2;
-        int carry = 0;
-        int sum;
-        int i;
+	int len2 = strlen(n2);
+	int max_len = (len1 > len2) ? len1 : len2;
+	int carry = 0;
+	int sum;
+	int i;
 
 	if (max_len + 1 > size_r)
-        {
-        	return 0;
-        }
+	{
+		return (0);
+	}
 
-        r[max_len + 1] = '0';
+	r[max_len + 1] = '0';
 
 	for (i = 0; i < max_len; i++)
 	{
-        	int digit1 = (i < len1) ? n1[len1 - 1 - i] - '0' : 0;
-        	int digit2 = (i < len2) ? n2[len2 - 1 - i] - '0' : 0;
+		int digit1 = (i < len1) ? n1[len1 - 1 - i] - '0' : 0;
+		int digit2 = (i < len2) ? n2[len2 - 1 - i] - '0' : 0;
 
-        	sum = digit1 + digit2 + carry;
-        	carry = sum / 10;
-        	r[max_len - i] = sum % 10 + '0';
-    	}
+		sum = digit1 + digit2 + carry;
+		carry = sum / 10;
+		r[max_len - i] = sum % 10 + '0';
+	}
 
 	if (carry)
-    	{
-        	r[0] = carry + '0';
-        	return (r);
-    	}
+	{
+		r[0] = carry + '0';
+		return (r);
+	}
 	else
-        {
-        	return (r + 1);
+	{
+		return (r + 1);
 	}
 }
