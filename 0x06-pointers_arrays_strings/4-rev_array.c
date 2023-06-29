b0cf617e158392824f115432c0cfafbecce938f7
eminentetim <etimeminent@gmail.com>
2023-06-29 12:21:11 -0700

    Creating a function that reverse a string.

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,14 +1,21 @@
 #include "main.h"
 
-void reverse_array(int* a, int n)
+/**
+ * reverse_array - reverse strings.
+ * @a: Pointer to the destination string.
+ * @n: hold the vale of src in bytes
+ * Return: Pointer to the resulting concatenated string.
+ */
+
+void reverse_array(int *a, int n)
 {
 	int j;
 	int rev;
 
-	for (j= 0; j < n / 2; j++)
+	for (j = 0; j < n / 2; j++)
 	{
 		rev = a[j];
 		a[j] = a[n - j - 1];
-		a[n -j - 1] =rev;
+		a[n - j - 1] = rev;
 	}
 }
