36b139a58391bae97f4db095ac574fe10f377b27
eminentetim <etimeminent@gmail.com>
2023-06-29 12:05:48 -0700

    Creating a function that compares two strings.

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _strcmp - Concatenates two strings.
+ * @s1: Pointer to the destination string.
+ * @s2: Pointer to the source string.
+ * Return: Pointer to the resulting concatenated string.
+ */
+
 int _strcmp(char *s1, char *s2)
 {
 	while (*s1 && *s2 && *s1 == *s2)
