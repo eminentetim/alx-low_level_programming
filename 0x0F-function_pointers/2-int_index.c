5c445810ad642e833c815f85ceedc4841f49970d
eminentetim <etimeminent@gmail.com>
2023-07-19 09:46:09 -0700

    Creating a function that searches for an integer.

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,7 +13,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
 
-	while(i < size)
+	while (i < size)
 	{
 		if (cmp(array[i]))
 		{
