a0712ab65bb76886b8dfbbd8b04e6c409644c012
etimeminent@gmail.com <etimeminent@gmail.com>
2024-02-07 03:13:13 -0800

    Searchin algorithms

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,6 +1,5 @@
 /*
  * File: 1-binary.c
- * Auth: Brennan D Baraban
  */
 
 #include "search_algos.h"
--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,6 +1,5 @@
 /*
  * File: 105-jump_list.c
- * Auth: Brennan D Baraban
  */
 
 #include "search_algos.h"
